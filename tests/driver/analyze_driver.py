ba251d56b5e75ffec2551d58b8e413eafff95d15
taylor-a-barnes <taylor.a.barnes@gmail.com>
2021-12-13 10:08:29 -0500

    Update analyze_driver.py

--- tests/driver/analyze_driver.py
+++ tests/driver/analyze_driver.py
@@ -30,30 +30,18 @@ final_name = mdi.MDI_Recv(mdi.MDI_NAME_LENGTH, mdi.MDI_CHAR, comm)
 assert initial_name == final_name
 print("Engine name: " + str(final_name))
 
-mdi.MDI_Send_Command("<CELL", comm)
-cell = mdi.MDI_Recv(9, mdi.MDI_DOUBLE, comm)
-print("Cell: " + str(cell))
-
-mdi.MDI_Send_Command("<CELL_DISPL", comm)
-cell_displ = mdi.MDI_Recv(3, mdi.MDI_DOUBLE, comm)
-print("Cell_displ: " + str(cell_displ))
-
 mdi.MDI_Send_Command("<NATOMS", comm)
 natoms = mdi.MDI_Recv(1, mdi.MDI_INT, comm)
 print("NAtoms: " + str(natoms))
-
-mdi.MDI_Send_Command("<COORDS", comm)
-coords = mdi.MDI_Recv(3*natoms, mdi.MDI_DOUBLE, comm)
-print("Coords: " + str(coords))
 
 mdi.MDI_Send_Command("<ENERGY", comm)
 energy = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, comm)
 print("Energy: " + str(energy))
 
-coords[0] += 0.1
-mdi.MDI_Send_Command(">COORDS", comm)
-mdi.MDI_Send(coords, 3*natoms, mdi.MDI_DOUBLE, comm)
-print("Coords: " + str(coords))
+#coords[0] += 0.1
+#mdi.MDI_Send_Command(">COORDS", comm)
+#mdi.MDI_Send(coords, 3*natoms, mdi.MDI_DOUBLE, comm)
+#print("Coords: " + str(coords))
 
 mdi.MDI_Send_Command("<ENERGY", comm)
 energy = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, comm)
@@ -61,7 +49,38 @@ print("Energy: " + str(energy))
 
 mdi.MDI_Send_Command("<FORCES", comm)
 forces = mdi.MDI_Recv(3*natoms, mdi.MDI_DOUBLE, comm)
-print("Forces: " + str(forces))
+#print("Forces: " + str(forces))
+
+mdi.MDI_Send_Command("<POLEDIMS", comm)
+polardim = mdi.MDI_Recv(1, mdi.MDI_INT, comm)
+#print("Polardim: " + str(polardim))
+
+# Get the polarizabilities
+mdi.MDI_Send_Command("<POLARITIES", comm)
+polarities = mdi.MDI_Recv(natoms, mdi.MDI_DOUBLE, comm)
+#print("Polarities: " + str(polarities))
+
+# Get the multipoles
+mdi.MDI_Send_Command("<MULTIPOLES", comm)
+multipoles = mdi.MDI_Recv(9*natoms, mdi.MDI_DOUBLE, comm)
+#print("Multipoles: " + str(multipoles))
+
+polarize = 1
+mdi.MDI_Send_Command(">POLARIZE", comm)
+mdi.MDI_Send(polarize, 1, mdi.MDI_INT, comm)
+
+#polarities[0] += 0.1
+mdi.MDI_Send_Command(">POLARITIES", comm)
+mdi.MDI_Send(polarities, natoms, mdi.MDI_DOUBLE, comm)
+
+#multipoles[6] += 0.1
+mdi.MDI_Send_Command(">MULTIPOLES", comm)
+mdi.MDI_Send(multipoles, 9*natoms, mdi.MDI_DOUBLE, comm)
+
+mdi.MDI_Send_Command("<ENERGY", comm)
+energy = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, comm)
+print("Energy: " + str(energy))
+
 
 print("CCC")
 
